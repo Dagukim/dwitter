881b9586a66171befc940d86a3173ee124cb574d
Dagukim <ikaros6388@gmail.com>
2024-03-26 23:57:10 +0900

    add firebase

---
+++ src/routes/firebases.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDpUrVhl76QWFeYOJrw8MQQI-TcLxHC6tI",
+    authDomain: "dwitter-41e8f.firebaseapp.com",
+    projectId: "dwitter-41e8f",
+    storageBucket: "dwitter-41e8f.appspot.com",
+    messagingSenderId: "1091958306459",
+    appId: "1:1091958306459:web:c83782eee4a34be175f83e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
