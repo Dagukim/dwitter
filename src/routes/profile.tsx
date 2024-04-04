94ea5200d41599bcff16e4341d52b9d8104554f6
Dagukim <ikaros6388@gmail.com>
2024-04-04 22:15:43 +0900

    editing user-avatar

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,74 @@
+import { auth, storage } from "@/firebase";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { updateProfile } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+const AvatarUpload = styled.label`
+    width: 80px;
+    overflow: hidden;
+    height: 80px;
+    border-radius: 50%;
+    background-color: #1d9bf0;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const AvatarImg = styled.img`
+    width: 100%;
+`;
+
+const AvatarInput = styled.input`
+    display: none;
+`;
+
+const Name = styled.span`
+    font-size: 22px;
+`;
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+    const user = auth.currentUser;
+    const [avatar, setAvatar] = useState(user?.photoURL);
+    const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = e.target;
+        if (!user) return;
+        if (files && files.length === 1) {
+            const file = files[0];
+            const locationRef = ref(storage, `user/${user.uid}/avatar`);
+            const result = await uploadBytes(locationRef, file);
+            const avatarUrl = await getDownloadURL(result.ref);
+            setAvatar(avatarUrl);
+            await updateProfile(user, { photoURL: avatarUrl });
+        }
+    };
+
+    return (
+        <Wrapper>
+            <AvatarUpload htmlFor="avatar">
+                {avatar ? (
+                    <AvatarImg src={avatar} />
+                ) : (
+                    <FontAwesomeIcon icon={faUser} size="2xl" />
+                )}
+            </AvatarUpload>
+            <AvatarInput
+                onChange={onAvatarChange}
+                type="file"
+                id="avatar"
+                accept="image/*"
+            />
+            <Name>{user?.displayName ?? "Anonymous"}</Name>
+        </Wrapper>
+    );
 }
